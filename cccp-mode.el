1504b8f1f91cca37213c0c50fe3b762c6f63a6ba
Craig Andera <candera@wangdera.com>
2011-11-18 16:57:16 -0500

    Get basic change detection working.

--- cccp-mode.el
+++ cccp-mode.el
@@ -11,27 +11,25 @@
 ;;
 ;; TODO: Write usage information
 
-(defvar cccp-history-buffer nil)
+(defvar cccp-last-before-change nil)
 
-(defun cccp-format-history (undo-list)
-  "Formats the provided undo-list into something human-readable."
-  (format "Not yet implemented: %d items in undo-list.\n" (length undo-list)))
+(defun cccp-before-change (beg end)
+  "Records the location of the change that is about to take place."
+  (setq cccp-last-before-change (list beg end (buffer-substring beg end))))
 
-(defun cccp-display-history ()
-  "Displays the current edit history in a *cccp-history* buffer."
-  (interactive)
-  (let ((undo-list buffer-undo-list))
-   (save-current-buffer
-     (set-buffer cccp-history-buffer)
-     (let ((buffer-read-only nil))
-       (goto-char (point-max))
-       (insert (cccp-format-history undo-list))))))
+(defun cccp-after-change (beg end len)
+  "Records the location of the change that just happened."
+  (let  ((after-text (buffer-substring beg end)))
+    (message "Text in buffer at position %d changed from '%s' to '%s'"
+             beg (third cccp-last-before-change) after-text)))
 
 (defvar cccp-mode-map (make-sparse-keymap)
   "Keymap for cccp-mode.")
 
 (define-minor-mode cccp-mode
-  "Toggle cccp-mode.
+  "Toggle cccp-mode, a minor mode for connecting to a CCCP server.
+
+CCCP is the Common Collaborative Coding Protocol.
 With no argument, this command toggles the mode. Non-null prefix
 argument turns on the mode. Null prefix argument turns off the
 mode.
@@ -40,7 +38,7 @@ When cccp-mode is enabled, edits in the buffer will be sent to
 other colloborators via the CCCP server, and edits received from
 the CCCP server will be applied to the buffer.
 
-Note tht cccp-mode is currently pre-alpha, i.e. completely
+Note that cccp-mode is currently pre-alpha, i.e. completely
 broken. Use at your own risk."
 
   ;; The indicator for the mode line.
@@ -49,17 +47,21 @@ broken. Use at your own risk."
   ;; The key bindings
   :keymap 'cccp-mode-map
 
-  ;; The body
+  ;; If we're turning off cccp-mode, we shouldn't grab before/after
+  ;; change notifications any more
+  (when (not cccp-mode)
+    (setq before-change-functions (remove 'cccp-before-change before-change-functions))
+    (setq after-change-functions (remove 'cccp-after-change after-change-functions)))
 
-  ;; Create the results buffer
-  (set (make-local-variable 'cccp-history-buffer)
-       (generate-new-buffer "*cccp-history*"))
-  (save-current-buffer
-    (set-buffer cccp-history-buffer)
-    (setq buffer-read-only t))
+  ;; Setup for when we're turning cccp-mode on
+  (when cccp-mode
+    ;; Set up the key bindings
+    (define-key cccp-mode-map (kbd "C-c /") 'cccp-display-history)
 
-  ;; Set up the key bindings
-  (define-key cccp-mode-map (kbd "C-c /") 'cccp-display-history)
-)
+    (make-local-variable 'cccp-last-before-change)
+
+    ;; Sign up to receive notification of changes to the buffer
+    (add-to-list 'before-change-functions 'cccp-before-change)
+    (add-to-list 'after-change-functions 'cccp-after-change)))
 
 (provide 'cccp-mode)
